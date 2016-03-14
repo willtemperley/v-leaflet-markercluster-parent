3d914548d2c8131522a3424cc3eb9407b1fd33ca
octavm <octavm@startmail.com>
2016-03-14 16:32:18 +0100

    Update LeafletMarkerClusterConnector.java

--- v-leaflet-markercluster/src/main/java/org/vaadin/addon/leaflet/markercluster/client/LeafletMarkerClusterConnector.java
+++ v-leaflet-markercluster/src/main/java/org/vaadin/addon/leaflet/markercluster/client/LeafletMarkerClusterConnector.java
@@ -1,8 +1,16 @@
 package org.vaadin.addon.leaflet.markercluster.client;
 
+import com.google.gwt.core.client.JavaScriptObject;
+import com.vaadin.client.MouseEventDetailsBuilder;
+import com.vaadin.client.communication.RpcProxy;
+import org.peimari.gleaflet.client.ClickListener;
 import org.peimari.gleaflet.client.LayerGroup;
+import org.peimari.gleaflet.client.MouseEvent;
 import org.vaadin.addon.leaflet.client.LeafletFeatureGroupConnector;
+import org.vaadin.addon.leaflet.client.U;
 import org.vaadin.addon.leaflet.markercluster.LMarkerClusterGroup;
+import org.vaadin.addon.leaflet.markercluster.shared.AnimationEndServerRpc;
+import org.vaadin.gleaflet.markercluster.client.AnimationEndListener;
 import org.vaadin.gleaflet.markercluster.client.MarkerClusterGroup;
 
 import com.vaadin.shared.ui.Connect;
@@ -10,6 +18,12 @@ import org.vaadin.gleaflet.markercluster.client.MarkerClusterGroupOptions;
 
 @Connect(LMarkerClusterGroup.class)
 public class LeafletMarkerClusterConnector extends LeafletFeatureGroupConnector {
+	
+private MarkerClusterGroup markerClusterGroup;
+
+    AnimationEndServerRpc animationEndServerRpc = RpcProxy.create(AnimationEndServerRpc.class, this);
+    
+    private JavaScriptObject clusterClickListener;
 
     public LeafletMarkerClusterState getState() {
         return (LeafletMarkerClusterState) super.getState();
@@ -17,8 +31,8 @@ public class LeafletMarkerClusterConnector extends LeafletFeatureGroupConnector 
 
 	@Override
 	protected LayerGroup createLayerGroup() {
-        MarkerClusterGroupOptions o = createOptions();
-		return MarkerClusterGroup.create(o);
+	MarkerClusterGroupOptions o = createOptions();
+		return markerClusterGroup = MarkerClusterGroup.create(o);
 	}
 
     protected MarkerClusterGroupOptions createOptions() {
@@ -57,6 +71,31 @@ public class LeafletMarkerClusterConnector extends LeafletFeatureGroupConnector 
         }
 
         return o;
+    }
+    
+    @Override
+    public void update() {
+		super.update();
+
+        if (clusterClickListener != null) {
+            markerClusterGroup.removeListener(clusterClickListener);
+            clusterClickListener = null;
+        }
+        clusterClickListener = markerClusterGroup.addClickListener(new ClickListener() {
+            @Override
+            public void onClick(MouseEvent event) {
+                rpc.onClick(U.toPoint(event.getLatLng()),
+                        MouseEventDetailsBuilder.buildMouseEventDetails(event.getNativeEvent(), getLeafletMapConnector().getWidget().getElement()));
+            }
+        });
+
+        markerClusterGroup.removeAnimationEndListener();
+        markerClusterGroup.addAnimationEndListener(new AnimationEndListener() {
+            @Override
+            public void onAnimationEnd() {
+                animationEndServerRpc.onAnimationEnd();
+            }
+        });
     }
 
 }
